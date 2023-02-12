eaf830201a8fc909cee46b6e940746c9674dc1f3
JomiXedYu <JomiXedYu@outlook.com>
2023-02-12 11:59:14 +0800

    csharp keywords

--- Source/Generator/Private/FGeneratorCore.cpp
+++ Source/Generator/Private/FGeneratorCore.cpp
@@ -718,8 +718,21 @@ FString FGeneratorCore::GetReturnParamName(FProperty* Property)
 
 FString FGeneratorCore::GetName(FString InName)
 {
-	static TSet<FName> KeyWords{TEXT("int")};
-
+	static TSet<FName> KeyWords{
+		TEXT("abstract"), TEXT("as"), TEXT("base"), TEXT("bool"), TEXT("break"), TEXT("byte"), TEXT("case"),
+		TEXT("catch"), TEXT("char"), TEXT("checked"), TEXT("class"), TEXT("const"), TEXT("continue"), TEXT("decimal"),
+		TEXT("default"), TEXT("delegate"), TEXT("do"), TEXT("double"), TEXT("else"), TEXT("enum"), TEXT("event"),
+		TEXT("explicit"), TEXT("extern"), TEXT("false"), TEXT("finally"), TEXT("fixed"), TEXT("float"), TEXT("for"),
+		TEXT("foreach"), TEXT("goto"), TEXT("if"), TEXT("implicit"), TEXT("in"), TEXT("int"),
+		TEXT("interface"), TEXT("internal"), TEXT("is"), TEXT("lock"), TEXT("long"), TEXT("namespace"), TEXT("new"),
+		TEXT("null"), TEXT("object"), TEXT("operator"), TEXT("out"),
+		TEXT("override"), TEXT("params"),
+		TEXT("private"), TEXT("protected"), TEXT("public"), TEXT("readonly"), TEXT("ref"), TEXT("return"), TEXT("sbyte"),
+		TEXT("sealed"), TEXT("short"), TEXT("sizeof"), TEXT("stackalloc"), TEXT("static"), TEXT("string"), TEXT("struct"),
+		TEXT("switch"), TEXT("this"), TEXT("throw"), TEXT("true"), TEXT("try"), TEXT("typeof"), TEXT("uint"),
+		TEXT("ulong"), TEXT("unchecked"), TEXT("unsafe"), TEXT("ushort"), TEXT("using"), TEXT("virtual"), TEXT("void"),
+		TEXT("volatile"), TEXT("while")
+	};
 	if (KeyWords.Contains(*InName))
 	{
 		return FString::Printf(TEXT("_%s"), *InName);
