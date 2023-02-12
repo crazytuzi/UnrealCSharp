a1874f92347ecb7c897b8e0773453be6be729aa1
crazytuzi <957824770@qq.com>
2023-02-13 01:24:42 +0800

    FUnrealCSharpFunctionLibrary

---
+++ Source/UnrealCSharpCore/Private/FUnrealCSharpFunctionLibrary.cpp
@@ -0,0 +1,65 @@
+﻿#include "FUnrealCSharpFunctionLibrary.h"
+
+FString FUnrealCSharpFunctionLibrary::GetProxyPath()
+{
+	return FPaths::Combine(FPaths::ProjectDir(), TEXT("Script"), TEXT("Proxy"));
+}
+
+FString FUnrealCSharpFunctionLibrary::GetGamePath()
+{
+	return FPaths::Combine(FPaths::ProjectDir(), TEXT("Script"), TEXT("Game"));
+}
+
+FString FUnrealCSharpFunctionLibrary::GetModuleName(const UField* InField)
+{
+	if (InField == nullptr)
+	{
+		return TEXT("");
+	}
+
+	auto ModuleName = InField->GetOuter() ? InField->GetOuter()->GetName() : TEXT("");
+
+	if (InField->IsNative())
+	{
+		ModuleName = ModuleName.Replace(TEXT("/Script/"), TEXT("/"));
+	}
+	else
+	{
+		auto Index = 0;
+
+		if (ModuleName.FindLastChar(TEXT('/'), Index))
+		{
+			ModuleName = ModuleName.Left(Index);
+		}
+	}
+
+	return ModuleName;
+}
+
+FString FUnrealCSharpFunctionLibrary::GetFileName(const UField* InField)
+{
+	auto ModuleName = GetModuleName(InField);
+
+	auto DirectoryName = FPaths::Combine(GetProxyPath(), ModuleName);
+
+	return FPaths::ConvertRelativePathToFull(FString::Printf(TEXT(
+			"%s.cs"
+		),
+	                                                         *FPaths::Combine(DirectoryName,
+	                                                                          InField->GetName()))
+	);
+}
+
+FString FUnrealCSharpFunctionLibrary::GetOldFileName(const UField* InField, const FString& OldName)
+{
+	auto ModuleName = GetModuleName(InField);
+
+	auto DirectoryName = FPaths::Combine(GetProxyPath(), ModuleName);
+
+	return FPaths::ConvertRelativePathToFull(FString::Printf(TEXT(
+			"%s.cs"
+		),
+	                                                         *FPaths::Combine(DirectoryName,
+	                                                                          OldName))
+	);
+}
