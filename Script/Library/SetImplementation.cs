736da4e391f8b61b0fdf46c7e07a7b48b048df0b
qingyingz <791846035@qq.com>
2023-04-08 15:36:01 +0800

    Revert "TSet迭代器"
    This reverts commit a71b52d151909103129e76807eef2d2716f5f2bb.

--- Script/Library/SetImplementation.cs
+++ Script/Library/SetImplementation.cs
@@ -26,25 +26,5 @@ namespace Script.Library
 
         [MethodImpl(MethodImplOptions.InternalCall)]
         public static extern Boolean Set_ContainsImplementation<T>(TSet<T> InSet, T InValue);
-        
-        
-        [MethodImpl(MethodImplOptions.InternalCall)]
-        public static extern Int32 Set_FindImplementation<T>(TSet<T>InSet,T Value);
-        
-        //     [MethodImpl(MethodImplOptions.InternalCall)]   
-        // static TArray<FProperty*> Set_ToArrayImplementation(const MonoObject* InMonoObject);
-        
-        [MethodImpl(MethodImplOptions.InternalCall)]
-        public static extern Int32 Set_GetMaxIndexImplementation<T>(TSet<T>InSet);
-        
-        //[MethodImpl(MethodImplOptions.InternalCall)]
-        // static TSet<void*> Set_UnionImplementation(const MonoObject* InMonoObject,TSet<void*> OtherSet);
-
-        [MethodImpl(MethodImplOptions.InternalCall)]
-        public static extern T Set_GetImplementation<T>(TSet<T>InSet, Int32 Index,out Object OutValue);
-
-        [MethodImpl(MethodImplOptions.InternalCall)]
-        public static extern Int32 Set_SetImplementation<T>(TSet<T>InSet,Int32 Index,T Value);
-        
     }
 }
--- Source/UnrealCSharp/Public/Reflection/Container/FSetHelper.h
+++ Source/UnrealCSharp/Public/Reflection/Container/FSetHelper.h
@@ -25,29 +25,10 @@ public:
 	int32 Remove(const void* InValue) const;
 
 	bool Contains(const void* InKey) const;
-	
-	int32 Find(const void* InValue) const;
 
-	TArray<FProperty*> ToArray()const;
-	
-	int32 GetMaxIndex() const;
-
-	TSet<void*> Union(TSet<void*> OtherSet) const;
-
-	void* Get(int32 Index) const;
-
-	void Set(int32 Index,void* InValue) const;
-
-	FPropertyDescriptor* GetElementPropertyDescriptor() const ;
-	
 	FProperty* GetElementProperty() const;
 
 	FScriptSet* GetScriptSet() const;
-
-	FORCEINLINE FScriptSetHelper CreateHelperFormElementProperty() const
-	{
-		return FScriptSetHelper::CreateHelperFormElementProperty(ElementPropertyDescriptor->GetProperty(), ScriptSet);
-	}
 
 private:
 	FPropertyDescriptor* ElementPropertyDescriptor;
