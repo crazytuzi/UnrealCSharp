aef96d9d336bc4093d12508719e850a49036b273
karrycharon <karrycharon@qq.com>
2023-04-17 14:35:54 +0800

    add generic method of GetDataTableRowFromName

---
+++ Source/UnrealCSharp/Public/Domain/InternalCall/UDataTableFunctionLibrary.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include "mono/metadata/object.h"
+
+class UDataTableFunctionLibraryImplementation
+{
+public:
+	static bool UDataTableFunctionLibrary_GetDataTableRowFromNameImplementation(MonoObject* InTable, MonoString* InRowName, MonoObject** OutRow);
+};
