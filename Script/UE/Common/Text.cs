bfd7b5903a7789179bcbf02474e3f648027cfbe5
crazytuzi <957824770@qq.com>
2023-10-08 20:37:30 +0800

    FName && FText && FString operator ==

--- Script/UE/Common/Text.cs
+++ Script/UE/Common/Text.cs
@@ -1,10 +1,16 @@
-﻿namespace Script.Common
+﻿using System;
+
+namespace Script.Common
 {
     public class FText
     {
         public FText(string InValue) => Value = InValue;
 
         public static implicit operator FText(string InValue) => new FText(InValue);
+
+        public static Boolean operator ==(FText A, FText B) => A.Value == B.Value;
+
+        public static Boolean operator !=(FText A, FText B) => A.Value != B.Value;
 
         public override string ToString() => Value;
 
