5bfa025dc2db2499bdd9b542bf4bca58a9f1e3f2
crazytuzi <957824770@qq.com>
2023-11-18 14:50:46 +0800

    Binding FFloatInterval && FInt32Interval

--- Source/UnrealCSharp/Private/Domain/InternalCall/TRegisterInterval.cpp
+++
@@ -1,5 +0,0 @@
-﻿#include "Domain/InternalCall/TRegisterInterval.h"
-
-static TRegisterInterval<FFloatInterval, float> RegisterFloatInterval;
-
-static TRegisterInterval<FInt32Interval, int32> RegisterInt32Interval;
