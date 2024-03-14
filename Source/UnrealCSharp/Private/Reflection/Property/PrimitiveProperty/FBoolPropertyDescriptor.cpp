3781d84e9cca2cdcb8ac5cd5a009342b5758a5bc
Jeffrey.Zhu <414927334@qq.com>
2024-03-14 20:54:40 +0800

    Property with bitField  (#319)
    * Set Primitive Property with BitField.
    
    * Just set reflection bool property with bitbield.
    
    ---------
    
    Co-authored-by: crazytuzi <liuxiangcode@gmail.com>
    Co-authored-by: jeffrey.zhu <zhujianfei@jedi-games.com>

--- Source/UnrealCSharp/Private/Reflection/Property/PrimitiveProperty/FBoolPropertyDescriptor.cpp
+++ Source/UnrealCSharp/Private/Reflection/Property/PrimitiveProperty/FBoolPropertyDescriptor.cpp
@@ -1,1 +1,9 @@
 ﻿#include "Reflection/Property/PrimitiveProperty/FBoolPropertyDescriptor.h"
+
+void FBoolPropertyDescriptor::Set(void* Src, void* Dest) const
+{
+	if (BoolProperty != nullptr)
+	{
+		BoolProperty->SetPropertyValue(Dest, *((bool*)Src));
+	}
+}
