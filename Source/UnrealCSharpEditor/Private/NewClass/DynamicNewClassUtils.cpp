1836fdae935a858fb32d9cf6fdcbd35a9f94daca
Srkmn <47887032@qq.com>
2025-03-16 20:28:31 +0800

    Fix FNewClassInfo Link Error

--- Source/UnrealCSharpEditor/Private/NewClass/DynamicNewClassUtils.cpp
+++ Source/UnrealCSharpEditor/Private/NewClass/DynamicNewClassUtils.cpp
@@ -10,6 +10,129 @@
 
 #define LOCTEXT_NAMESPACE "DynamicNewClassUtils"
 
+FDynamicNewClassInfo::FDynamicNewClassInfo(const FNewClassInfo& InNewClassInfo)
+	: ClassType(InNewClassInfo.ClassType)
+	  , BaseClass(InNewClassInfo.BaseClass)
+{
+}
+
+FText FDynamicNewClassInfo::GetClassName() const
+{
+	switch (ClassType)
+	{
+	case FNewClassInfo::EClassType::UObject:
+		{
+			return BaseClass ? BaseClass->GetDisplayNameText() : FText::GetEmpty();
+		}
+	case FNewClassInfo::EClassType::EmptyCpp:
+		{
+			return LOCTEXT("NoParentClass", "None");
+		}
+	case FNewClassInfo::EClassType::SlateWidget:
+		{
+			return LOCTEXT("SlateWidgetParentClass", "Slate Widget");
+		}
+	case FNewClassInfo::EClassType::SlateWidgetStyle:
+		{
+			return LOCTEXT("SlateWidgetStyleParentClass", "Slate Widget Style");
+		}
+	case FNewClassInfo::EClassType::UInterface:
+		{
+			return LOCTEXT("UInterfaceParentClass", "Unreal Interface");
+		}
+	default:
+		break;
+	}
+
+	return FText::GetEmpty();
+}
+
+FString FDynamicNewClassInfo::GetClassNameCPP() const
+{
+	switch (ClassType)
+	{
+	case FNewClassInfo::EClassType::UObject:
+		{
+			return BaseClass ? BaseClass->GetName() : TEXT("");
+		}
+	case FNewClassInfo::EClassType::EmptyCpp:
+		{
+			return TEXT("");
+		}
+	case FNewClassInfo::EClassType::SlateWidget:
+		{
+			return TEXT("CompoundWidget");
+		}
+	case FNewClassInfo::EClassType::SlateWidgetStyle:
+		{
+			return TEXT("SlateWidgetStyle");
+		}
+	case FNewClassInfo::EClassType::UInterface:
+		{
+			return TEXT("Interface");
+		}
+	default:
+		break;
+	}
+
+	return TEXT("");
+}
+
+FText FDynamicNewClassInfo::GetClassDescription(const bool bFullDescription) const
+{
+	switch (ClassType)
+	{
+	case FNewClassInfo::EClassType::UObject:
+		{
+			if (BaseClass)
+			{
+				auto ClassDescription = BaseClass->GetToolTipText(!bFullDescription).ToString();
+
+				if (!bFullDescription)
+				{
+					if (auto FullStopIndex = 0; ClassDescription.FindChar('.', FullStopIndex))
+					{
+						ClassDescription.LeftInline(FullStopIndex + 1,
+#if UE_F_STRING_LEFT_CHOP_IN_LINE_E_ALLOW_SHRINKING
+						                            EAllowShrinking::No
+#else
+													false
+#endif
+						);
+					}
+
+					ClassDescription.ReplaceInline(TEXT("\n"), TEXT(" "), ESearchCase::CaseSensitive);
+				}
+
+				return FText::FromString(ClassDescription);
+			}
+		}
+		break;
+	case FNewClassInfo::EClassType::EmptyCpp:
+		{
+			return LOCTEXT("EmptyClassDescription", "An empty C++ class with a default constructor and destructor.");
+		}
+	case FNewClassInfo::EClassType::SlateWidget:
+		{
+			return LOCTEXT("SlateWidgetClassDescription", "A custom Slate widget, deriving from SCompoundWidget.");
+		}
+	case FNewClassInfo::EClassType::SlateWidgetStyle:
+		{
+			return LOCTEXT("SlateWidgetStyleClassDescription",
+			               "A custom Slate widget style, deriving from FSlateWidgetStyle, along with its associated UObject wrapper class.");
+		}
+	case FNewClassInfo::EClassType::UInterface:
+		{
+			return LOCTEXT("UInterfaceClassDescription",
+			               "A UObject Interface class, to be implemented by other UObject-based classes.");
+		}
+	default:
+		break;
+	}
+
+	return FText::GetEmpty();
+}
+
 void FDynamicNewClassUtils::OpenAddDynamicClassToProjectDialog(const FString& InInitialPath)
 {
 	const auto WindowTitle = LOCTEXT("AddCodeWindowHeader_DynamicCppClass", "Add Dynamic C# Class");
@@ -157,123 +280,6 @@ FString FDynamicNewClassUtils::GetAssetGeneratedClassName(const FAssetData& InAs
 	}
 
 	return GeneratedClass;
-}
-
-FString FNewClassInfo::GetClassNameCPP() const
-{
-	switch (ClassType)
-	{
-	case EClassType::UObject:
-		{
-			return BaseClass ? BaseClass->GetName() : TEXT("");
-		}
-	case EClassType::EmptyCpp:
-		{
-			return TEXT("");
-		}
-	case EClassType::SlateWidget:
-		{
-			return TEXT("CompoundWidget");
-		}
-	case EClassType::SlateWidgetStyle:
-		{
-			return TEXT("SlateWidgetStyle");
-		}
-	case EClassType::UInterface:
-		{
-			return TEXT("Interface");
-		}
-	default:
-		break;
-	}
-
-	return TEXT("");
-}
-
-FText FNewClassInfo::GetClassName() const
-{
-	switch (ClassType)
-	{
-	case EClassType::UObject:
-		{
-			return BaseClass ? BaseClass->GetDisplayNameText() : FText::GetEmpty();
-		}
-	case EClassType::EmptyCpp:
-		{
-			return LOCTEXT("NoParentClass", "None");
-		}
-	case EClassType::SlateWidget:
-		{
-			return LOCTEXT("SlateWidgetParentClass", "Slate Widget");
-		}
-	case EClassType::SlateWidgetStyle:
-		{
-			return LOCTEXT("SlateWidgetStyleParentClass", "Slate Widget Style");
-		}
-	case EClassType::UInterface:
-		{
-			return LOCTEXT("UInterfaceParentClass", "Unreal Interface");
-		}
-	default:
-		break;
-	}
-
-	return FText::GetEmpty();
-}
-
-FText FNewClassInfo::GetClassDescription(const bool bFullDescription) const
-{
-	switch (ClassType)
-	{
-	case EClassType::UObject:
-		{
-			if (BaseClass)
-			{
-				auto ClassDescription = BaseClass->GetToolTipText(!bFullDescription).ToString();
-
-				if (!bFullDescription)
-				{
-					if (auto FullStopIndex = 0; ClassDescription.FindChar('.', FullStopIndex))
-					{
-						ClassDescription.LeftInline(FullStopIndex + 1,
-#if UE_F_STRING_LEFT_CHOP_IN_LINE_E_ALLOW_SHRINKING
-						                            EAllowShrinking::No
-#else
-						                            false
-#endif
-						);
-					}
-
-					ClassDescription.ReplaceInline(TEXT("\n"), TEXT(" "), ESearchCase::CaseSensitive);
-				}
-
-				return FText::FromString(ClassDescription);
-			}
-		}
-		break;
-	case EClassType::EmptyCpp:
-		{
-			return LOCTEXT("EmptyClassDescription", "An empty C++ class with a default constructor and destructor.");
-		}
-	case EClassType::SlateWidget:
-		{
-			return LOCTEXT("SlateWidgetClassDescription", "A custom Slate widget, deriving from SCompoundWidget.");
-		}
-	case EClassType::SlateWidgetStyle:
-		{
-			return LOCTEXT("SlateWidgetStyleClassDescription",
-			               "A custom Slate widget style, deriving from FSlateWidgetStyle, along with its associated UObject wrapper class.");
-		}
-	case EClassType::UInterface:
-		{
-			return LOCTEXT("UInterfaceClassDescription",
-			               "A UObject Interface class, to be implemented by other UObject-based classes.");
-		}
-	default:
-		break;
-	}
-
-	return FText::GetEmpty();
 }
 
 UClass* FDynamicNewClassUtils::GetAncestorClass(const UClass* InClass)
